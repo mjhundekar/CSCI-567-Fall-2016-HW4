b4d556bf04068b69736fece6f93b6751cf60ceb3
mjhundekar <mohammedjunaidh@gmail.com>
2016-10-22 19:30:48 -0700

    fixed . to ,

--- CSCI567_hw4_fall16.py
+++ CSCI567_hw4_fall16.py
@@ -10,7 +10,7 @@ norm_x_train, norm_x_test = normalize(x_train, x_test);
 
 achitecture_1 = [[50,2],[50,50,2],[50,50,50,2],[50,50,50,50,2]]
 
-achitecture_2 = [[50,50,2],[50,500,2],[50,500,300,2],[50,800,500,300,2]. [50, 800, 800, 500, 300, 2]]
+achitecture_2 = [[50,50,2],[50,500,2],[50,500,300,2],[50,800,500,300,2], [50, 800, 800, 500, 300, 2]]
 
 testmodels(norm_x_train, y_train, norm_x_test, y_test, achitecture_1, actfn='linear', last_act='softmax', reg_coeffs=[0.0],
                 num_epoch=30, batch_size=1000, sgd_lr=1e-5, sgd_decays=[0.0], sgd_moms=[0.0],
