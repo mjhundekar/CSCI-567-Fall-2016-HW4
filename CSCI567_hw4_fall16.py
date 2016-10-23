001daf64404ff66baf04de11021e989407943854
mjhundekar <mohammedjunaidh@gmail.com>
2016-10-22 18:31:31 -0700

    Implemented load data

---
+++ CSCI567_hw4_fall16.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 22 18:17:40 2016
+
+@author: mjhundekar
+"""
+from hw_utils import *
+x_train, y_train, x_test, y_test = loaddata(MiniBooNE_PID.txt);
