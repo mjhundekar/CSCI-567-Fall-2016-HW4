449be0427275d6be088266109d5b26813e2226aa
mjhundekar <mohammedjunaidh@gmail.com>
2016-10-23 01:50:02 +0000

    Fixed missing quotes

--- CSCI567_hw4_fall16.py
+++ CSCI567_hw4_fall16.py
@@ -5,4 +5,4 @@ Created on Sat Oct 22 18:17:40 2016
 @author: mjhundekar
 """
 from hw_utils import *
-x_train, y_train, x_test, y_test = loaddata(MiniBooNE_PID.txt);
+x_train, y_train, x_test, y_test = loaddata('MiniBooNE_PID.txt');
